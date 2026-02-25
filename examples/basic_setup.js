2975ddb4d0665b3dd508db2d65f6d60ad9b7273f
David J. kordsmeier <dkords@gmail.com>
2026-02-24 23:34:42 -0800

    refs #7 Get example cleaned up to remove spinner, no weird floors
    Tried to add a little debug output to the text but need to get the camera position updating

--- examples/basic_setup.js
+++ examples/basic_setup.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
-import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
+import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { BoxLineGeometry } from "three/examples/jsm/geometries/BoxLineGeometry.js";
+import { TeapotGeometry } from "three/examples/jsm/geometries/TeapotGeometry.js";
 
 import ThreeMeshUI from "../src/three-mesh-ui.js";
 
@@ -14,6 +15,7 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
 let scene, camera, renderer, controls;
+let campos = new THREE.Vector3();
 
 window.addEventListener("load", init);
 window.addEventListener("resize", onWindowResize);
@@ -22,43 +24,106 @@ window.addEventListener("resize", onWindowResize);
 
 function init() {
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x505050);
 
-	camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 0.1, 100);
+	// Uncommont to change all background to match this color
+	// scene.background = new THREE.Color(0x505050);
 
+	// camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 0.1, 100);
+	camera = new THREE.PerspectiveCamera(
+		50,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		10,
+	);
+	camera.position.set(0, 1.6, 3);
+	campos.setFromMatrixPosition( camera.matrixWorld );
+	
 	renderer = new THREE.WebGLRenderer({
 		antialias: true,
+		alpha: true,
 	});
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(WIDTH, HEIGHT);
 	renderer.xr.enabled = true;
-	document.body.appendChild(VRButton.createButton(renderer));
+	// document.body.appendChild(VRButton.createButton(renderer));
+	const arButton = ARButton.createButton(renderer, {
+			// requiredFeatures: ["hit-test"], // Optional: Add if you need hit-testing for AR placement
+			optionalFeatures: ["dom-overlay"],
+			sessionInit: {
+				// Explicitly request local-floor here too
+				requiredFeatures: [
+					"local-floor",
+					"bounded-floor",
+					"hand-tracking",
+					"layers",
+				],
+			},
+	});
+	document.body.appendChild(arButton);
 	document.body.appendChild(renderer.domElement);
+	renderer.xr.setReferenceSpaceType("local-floor");
 
 	controls = new OrbitControls(camera, renderer.domElement);
-	camera.position.set(0, 1.6, 0);
+	// camera.position.set(0, 3.0, 0);
+	camera.position.set(0, 2.6, 0);
+	// camera.position.set(0, 1.6, 0);
+
+	/*
+	const cameraGroup = new THREE.Group();
+	cameraGroup.position.set(0, -1, 1.5); // Set the initial VR Headset Position.
+	*/
 	controls.target = new THREE.Vector3(0, 1, -1.8);
 	controls.update();
 
 	// ROOM
 
 	const room = new THREE.LineSegments(
-		new BoxLineGeometry(6, 6, 6, 10, 10, 10).translate(0, 3, 0),
+		new BoxLineGeometry(6, 6, 6, 10, 10, 10).translate(0, 2, 0), // Floor height offset is the Y component in this scene
 		new THREE.LineBasicMaterial({ color: 0x808080 }),
 	);
 
 	scene.add(room);
 
+	// Sunshine
+	const sun = new THREE.DirectionalLight(0xffffcc);
+	sun.position.set(0, 1, 0);
+	scene.add(sun);
+
+	// Teapot
+	// TODO: Improve this: https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_teapot.html
+	const geometry = new TeapotGeometry(0.15, 18).translate(0, 1, 0);
+	const material = new THREE.MeshBasicMaterial({
+		wireframe: true,
+		color: 0x00ff00,
+	}); // new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	const teapot = new THREE.Mesh(geometry, material);
+	scene.add(teapot);
 	// TEXT PANEL
 
 	makeTextPanel();
-
+	makePictureFrame(0, 0.5, -3);
 	//
 
 	renderer.setAnimationLoop(loop);
 }
 
 //
+// Scene Utilities
+//
+
+function makePictureFrame(xoffset, yoffset, zoffset) {
+	const points = [];
+	points.push(new THREE.Vector3(-1 + xoffset, 1 + yoffset, 0 + zoffset));
+	points.push(new THREE.Vector3(1 + xoffset, 1 + yoffset, 0 + zoffset));
+	points.push(new THREE.Vector3(1 + xoffset, -1 + yoffset, 0 + zoffset));
+	points.push(new THREE.Vector3(-1 + xoffset, -1 + yoffset, 0 + zoffset));
+	points.push(new THREE.Vector3(-1 + xoffset, 1 + yoffset, 0 + zoffset)); // Close the loop
+
+	const geometry = new THREE.BufferGeometry().setFromPoints(points);
+	const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+	const line = new THREE.LineLoop(geometry, material); // Or THREE.LineSegments
+	scene.add(line);
+}
 
 function makeTextPanel() {
 	const container = new ThreeMeshUI.Block({
@@ -72,7 +137,9 @@ function makeTextPanel() {
 		// interLine: 0,
 	});
 
-	container.position.set(0, 1, -1.8);
+	
+	// container.position.set(0, 1, -1.8);
+	container.position.set(0, 0, -1.8);
 	container.rotation.x = -0.55;
 	scene.add(container);
 
@@ -87,12 +154,20 @@ function makeTextPanel() {
 
 		new ThreeMeshUI.Text({
 			content:
-				" As well as multi font size lines with consistent vertical spacing",
+				" As well as multi font size lines with consistent vertical spacing : ",
 			fontSize: 0.08,
 		}),
+		new ThreeMeshUI.Text({
+			content: "x " + campos.x + " y " + campos.y + " z " + campos.z,
+			fontSize: 0.03,
+		}),
+
 	);
 
+
 	return;
+	// Uncomment to apply this effect
+	/*
 	container.onAfterUpdate = function () {
 		console.log(container.lines);
 
@@ -100,7 +175,7 @@ function makeTextPanel() {
 
 		console.log("lines", container.lines);
 
-		var plane = new Mesh(
+		const plane = new Mesh(
 			new PlaneGeometry(container.lines.width, container.lines.height),
 			new MeshBasicMaterial({ color: 0xff9900 }),
 		);
@@ -120,6 +195,7 @@ function makeTextPanel() {
 
 		container.add(plane);
 	};
+	*/
 }
 
 // handles resizing the renderer when the viewport is resized
@@ -139,5 +215,7 @@ function loop() {
 	ThreeMeshUI.update();
 
 	controls.update();
+	// console.log(campos);
+	
 	renderer.render(scene, camera);
 }
